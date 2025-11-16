fea10bf59bac8c20e32a71d179d5a5eff64c8811
EnochBoss <enoch.bosomtwe@gmail.com>
2025-11-16 22:39:07 +0000

    Add postcard component and include props

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+
+interface UsersPageProps {
+  users: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ users }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+      {users.map((user) => (
+        <UserCard key={user.id} {...user} />
+      ))}
+    </div>
+  );
+};
+
+export default Users;
